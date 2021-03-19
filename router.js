8a69777877a18affe8b0ff12483c2cad6e8e93b3
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-18 22:50:05 -0400

    Added root?

--- router.js
+++ router.js
@@ -2,6 +2,11 @@ let express = require('express')
 let router = new express.Router()
 let vedicModel = require('./db/vedicModel')
 
+router.get('/', (request, response) => {
+    vedicModel.find({})
+        .then((data) => response.json(data))
+})
+
 router.get('/:category', (request, response) => {
     vedicModel.find({ category: request.params.category })
         .then((data) => {
