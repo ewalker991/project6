5a2d634c7ce606f1082c823af896b15fc25942e2
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-19 09:12:26 -0400

    Changed server.js port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let app = require('./app.js')
-let server = app.listen(9001)
+let server = app.listen(process.env.PORT || 9001)
 
 server.on('listening', () => console.log("Server is listening..."))
 server.on('error', error => console.error("OH NO I'M BROKEN", error))
