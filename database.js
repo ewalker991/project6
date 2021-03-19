a57d964314a3fe2d921eb8756d3b9d72ccf7a137
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-18 22:22:43 -0400

    Uhhh...

--- database.js
+++ database.js
@@ -2,4 +2,14 @@ let mongoose = require('mongoose')
 
 let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
-mongoose.connect("mongodb://localhost/project6", mongooseConnectionConfig)
+let connectionString = "";
+console.log(process.env.NODE_ENV, "env")
+if (process.env.NODE_ENV === "production") {
+    connectionString = process.env.DB_URL;
+} else {
+    connectionString = "mongodb://localhost/project6";
+}
+
+console.log(connectionString)
+
+mongoose.connect(connectionString, mongooseConnectionConfig)
