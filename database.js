a469a03747a496e876b1133823a544f9a1ef67c6
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-18 22:57:03 -0400

    Changed connection string.

--- database.js
+++ database.js
@@ -2,13 +2,15 @@ let mongoose = require('mongoose')
 
 let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
-let connectionString = "";
+let connectionString = process.env.DB_URL;
 console.log(process.env.NODE_ENV, "env")
-if (process.env.NODE_ENV === "production") {
-    connectionString = process.env.DB_URL;
-} else {
-    connectionString = "mongodb://localhost/project6";
-}
+// if (process.env.NODE_ENV === "production") {
+//     connectionString = process.env.DB_URL;
+// } else {
+//     connectionString = "mongodb://localhost/project6";
+// }
+
+
 
 console.log(connectionString)
 
