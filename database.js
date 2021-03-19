0e480623ff71858c1419bca901a46b5167f72a59
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-18 23:25:01 -0400

    Uncommented some lines in database.js

--- database.js
+++ database.js
@@ -4,11 +4,11 @@ let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true
 
 let connectionString = process.env.DB_URL;
 console.log(process.env.NODE_ENV, "env")
-// if (process.env.NODE_ENV === "production") {
-//     connectionString = process.env.DB_URL;
-// } else {
-//     connectionString = "mongodb://localhost/project6";
-// }
+if (process.env.NODE_ENV === "production") {
+    connectionString = process.env.DB_URL;
+} else {
+    connectionString = "mongodb://localhost/project6";
+}
 
 
 
