d912f6a16499a168fcdf478c8d4cb6e09fec5db8
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-22 16:21:33 -0400

    Touched up CRUD function for '/' directory

--- router.js
+++ router.js
@@ -1,6 +1,13 @@
 let express = require('express')
 let router = new express.Router()
 let vedicRouter = require('./db/router.js')
+let vedicModel = require('./db/vedicModel')
+
+
+router.get('/', (request, response) => {
+    vedicModel.find({})
+        .then((data) => response.json(data))
+})
 
 router.use('/', vedicRouter)
 
